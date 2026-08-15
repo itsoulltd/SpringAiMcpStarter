8843c22b3dc6d9d04fcd6734828d1df419265a56
Towhid Islam <m.towhid.islam@gmail.com>
2026-08-15 16:54:29 +0200

    init:786

---
+++ src/main/java/com/infoworks/mcp/SpringAiMcpStarterApplication.java
@@ -0,0 +1,13 @@
+package com.infoworks.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMcpStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infoworks/mcp/SpringAiMcpStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoworks.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
