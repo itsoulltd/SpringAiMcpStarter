fdd328418150c44a47a74cfad42f80787c3427f5
Towhid Islam <m.towhid.islam@gmail.com>
2026-08-15 19:05:55 +0200

    fix: updated

--- src/main/java/com/infoworks/mcp/domain/entities/Auditable.java
+++ src/main/java/com/infoworks/mcp/domain/entities/Auditable.java
@@ -1,25 +1,32 @@
 package com.infoworks.mcp.domain.entities;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.infoworks.entity.Entity;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class Auditable<ID, VERSION> extends Persistable<ID, VERSION> {
+public class Auditable<ID, VERSION> extends Entity implements Externalizable {
 
     @CreatedDate
     @Column(name = "created_date")
-    LocalDate createdDate;
+    LocalDateTime createdDate;
 
     @LastModifiedDate
     @Column(name = "last_modified_date")
-    LocalDate lastModifiedDate;
+    LocalDateTime lastModifiedDate;
 
     @AttributeOverride(name = "username", column = @Column(name = "created_by"))
     @Embedded @CreatedBy
@@ -29,4 +36,25 @@ public class Auditable<ID, VERSION> extends Persistable<ID, VERSION> {
     @Embedded @LastModifiedBy
     Username lastModifiedBy;
 
+    @Version @JsonIgnore
+    private VERSION version;
+
+    public VERSION getVersion() {
+        return version;
+    }
+
+    public void setVersion(VERSION version) {
+        this.version = version;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(marshalling(true));
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        Map<String, Object> data = (Map<String, Object>) in.readObject();
+        unmarshalling(data, true);
+    }
 }
