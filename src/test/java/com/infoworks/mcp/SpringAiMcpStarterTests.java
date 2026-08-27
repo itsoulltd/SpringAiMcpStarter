67c2d9f48aa939e8ddcb1e9376bea34ab0e253ed
m.towhid.islam <m.towhid.islam@gmail.com>
2026-08-27 16:00:19 +0200

    fix: adding test and logback config

--- src/test/java/com/infoworks/mcp/SpringAiMcpStarterTests.java
+++ src/test/java/com/infoworks/mcp/SpringAiMcpStarterTests.java
@@ -1,13 +1,17 @@
 package com.infoworks.mcp;
 
+import com.infoworks.mcp.config.TestJPAH2Config;
+import com.infoworks.mcp.webapp.config.BeanConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {SpringAiMcpStarterTests.class, BeanConfig.class, TestJPAH2Config.class})
+//@ComponentScan(basePackages = {"com.infoworks.mcp.controllers", "com.infoworks.mcp.domain", "com.infoworks.mcp.services"})
 class SpringAiMcpStarterTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Loaded");
 	}
 
 }
